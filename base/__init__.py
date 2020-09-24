2e241fee2fe0c27fe65134ec095885572026fc4e
Rohit Patil <rahulhimesh09@gmail.com>
2020-09-24 16:15:04 +0530

    added flask bootstrap support

--- base/__init__.py
+++ base/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['secret_key'] = '7e752171d960ffdaf5e5e0490b7684038e2265e444f4def378d923984f3b8400'
+Bootstrap(app)
 
 from . import routes
