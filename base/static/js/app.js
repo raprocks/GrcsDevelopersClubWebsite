5491058ffd741956bf4e554349214eed33c09292
Rohit Patil <rahulhimesh09@gmail.com>
2020-09-24 15:41:26 +0530

    changes to style sheet

---
+++ base/static/js/app.js
@@ -0,0 +1,9 @@
+const navSlide = () => {
+    const burger = document.querySelector('.hamburger');
+    const nav = document.querySelector('.nav-links');
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-action');
+    });
+}
+
+navSlide();
